711373c6838133b60e57b03fe1a7fc2ac6b2f54b
wanglihui <lihui.wang@jingli.tech>
2016-10-10 11:05:07 +0800

    init

---
+++ index.js
@@ -0,0 +1,40 @@
+/**
+ * Created by wlh on 2016/10/10.
+ */
+'use strict';
+
+
+exports.toHanzi = function (money) {
+  if (typeof money != 'number' ) {
+    throw new Error('金额只支持数字格式')
+  }
+
+  if (money >= Math.pow(10, 17)) {
+    throw new Error('目前只支持Math.pow(10, 17)以内数字转换');
+  }
+
+  let vals = ['零','壹','贰','叁','肆','伍','陆','柒','捌','玖']
+  let units = [
+    '分', '角',
+    '元', '拾', '佰', '仟',
+    '万', '拾', '佰', '仟',
+    '亿', '拾', '佰', '仟',
+    '兆', '拾', '佰', '仟'
+  ]
+  //将小数转换为整数
+  money = Math.floor(money * 100)
+  let str = '';
+  let idx = 0;
+  do {
+    let num = money % 10;
+    //每处理一位,将此位从数字中删除,如 1001,处理完1后删除,剩余100再以此处理
+    money = Math.floor( money / 10 );
+    let unit = units[idx++];
+    str = vals[num] + unit + str;
+    if (money <= 0) {
+      break;
+    }
+  } while(true);
+  str += '整';
+  return str;
+}
